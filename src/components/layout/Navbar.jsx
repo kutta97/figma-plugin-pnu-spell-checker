3c1fd2a9a62452a8dd0075a864e01bc5629f38e1
kutta97 <yhjin04170@gmail.com>
2023-02-01 01:21:52 +0900

    feat: Layout 컴포넌트에 style 적용

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Info from '../../assets/icons/ic_info.svg';
+import Filter from '../../assets/icons/ic_filter.svg';
+import { Font16W700 } from '../../assets/styles/fonts';
 
-export function Navbar({ name }) {
+export function Navbar({ title }) {
   return (
     <NavbarStyled>
-      <h2>{name}</h2>
+      <Info width={24} height={24} />
+      <h2>{title}</h2>
+      <Filter width={24} height={24} />
     </NavbarStyled>
   );
 }
 
-const NavbarStyled = styled.header``;
+const NavbarStyled = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 14px 16px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
+
+  h2 {
+    ${Font16W700}
+  }
+`;
