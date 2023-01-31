9e76dc7c91b140e65b5d964516b66018fbbae03a
kutta97 <yhjin04170@gmail.com>
2023-02-01 01:16:27 +0900

    feat: Font Style 정의

---
+++ src/assets/styles/fonts.js
@@ -0,0 +1,98 @@
+import { css } from 'styled-components';
+
+/** font-size: 18 */
+export const Font18W700 = css`
+  ${({ theme }) => theme.fontSizes.s18}
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+`;
+
+/** font-size: 16 */
+export const Font16W700 = css`
+  ${({ theme }) => theme.fontSizes.s16}
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+`;
+
+/** font-size: 15 */
+export const Font15W600 = css`
+  ${({ theme }) => theme.fontSizes.s15}
+  font-style: normal;
+  font-weight: 600;
+  text-align: center;
+`;
+
+export const Font15W500 = css`
+  ${({ theme }) => theme.fontSizes.s15}
+  font-style: normal;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Font15W400 = css`
+  ${({ theme }) => theme.fontSizes.s15}
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+`;
+
+/** font-size: 14 */
+export const Font14W600 = css`
+  ${({ theme }) => theme.fontSizes.s14}
+  font-style: normal;
+  font-weight: 600;
+  text-align: center;
+`;
+
+export const Font14W500 = css`
+  ${({ theme }) => theme.fontSizes.s14}
+  font-style: normal;
+  font-weight: 500;
+  text-align: center;
+`;
+
+/** font-size: 13 */
+export const Font13W700 = css`
+  ${({ theme }) => theme.fontSizes.s13}
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const Font13W600 = css`
+  ${({ theme }) => theme.fontSizes.s13}
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const Font13W500 = css`
+  ${({ theme }) => theme.fontSizes.s13}
+  font-style: normal;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Font13W400 = css`
+  ${({ theme }) => theme.fontSizes.s13}
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+`;
+
+/** font-size: 12 */
+export const Font12W500 = css`
+  ${({ theme }) => theme.fontSizes.s12}
+  font-style: normal;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Font12W400 = css`
+  ${({ theme }) => theme.fontSizes.s12}
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+`;
