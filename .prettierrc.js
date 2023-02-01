acead82fd3d732c3581ef3fb0926fbb8965e8c43
kutta97 <yhjin04170@gmail.com>
2023-02-01 23:56:15 +0900

    feat: .prettierrc 에 importOrder 옵션 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,17 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+
+  importOrder: [
+    '^@Assets/(.*)$',
+    '^@Components/(.*)$',
+    '^@Data/(.*)$',
+    '^@Hooks/(.*)$',
+    '^@Services/(.*)$',
+    '^@Store/(.*)$',
+    '^@Views/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
