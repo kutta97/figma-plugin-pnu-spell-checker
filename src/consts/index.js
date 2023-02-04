cd35724f0c2cb5192949f81f28bae3f179d9d962
mos <ysg01129@naver.com>
2023-02-04 12:24:41 +0900

    feat: Define type for page

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,5 +1,5 @@
 export const PAGE_TITLE = {
-  MAIN: '맞춤법 검사하기',
+  HOME: '맞춤법 검사하기',
   SEARCHING: '검사 중...',
   DETAIL: '검사 상세',
   RESULT: '검사 결과',
@@ -10,10 +10,3 @@ export const PAGE_TYPE = Object.keys(PAGE_TITLE).reduce((acc, key) => {
   acc[key] = key;
   return acc;
 }, {});
-
-export const BOTTOM_PAGE_TYPE = {
-  MAIN: 'MAIN',
-  CONVERT: 'CONVERT',
-  GO_MAIN: 'GO_MAIN',
-  HIDDEN: 'HIDDEN',
-};
