6277812427147d9968632585d5f5d709adf90375
mos <ysg01129@naver.com>
2023-02-10 00:04:31 +0900

    feat: Add reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,9 @@
 import { homeReducer } from '@store/home';
+import { resultReducer } from '@store/result';
 import { rootReducer } from '@store/root';
 
 export const reducers = {
   homeReducer,
   rootReducer,
+  resultReducer,
 };
