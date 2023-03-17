dde9220f94e39668c9b571e204dedccd9f37ccdf
kutta97 <yhjin04170@gmail.com>
2023-03-17 13:01:54 +0900

    feat: selectionchange 이벤트 발생 시 nodeStore 의 state 변경로직 추가

--- src/views/home/homeVM.js
+++ src/views/home/homeVM.js
@@ -1,21 +1,41 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
+
+import { fromMessage } from '@utils/messages';
 
 import { searchListenerConnect } from '@store/home';
 import { homeFilterSelector } from '@store/home/selector';
 import { useAppDispatch, useAppSelector } from '@store/hook';
+import { setSelectedNodes, totalSelectedNodeCountSelector } from '@store/node';
 
 export const useHomeVM = () => {
   const dispatch = useAppDispatch();
   const filtered = useAppSelector(homeFilterSelector);
+  const totalSelectedNodeCount = useAppSelector(totalSelectedNodeCountSelector);
 
-  // TODO 선택한 frame 없을 경우 disabled 하게 변경
-  const searchDisabled = useMemo(() => {
-    return true;
-  }, []);
+  const getMessage = async (e) => {
+    const data = await fromMessage(e);
+    if (data.pluginMessage.query !== 'selectionchange') return;
+    dispatch(setSelectedNodes(data.pluginMessage.selectedNodesWithText));
+  };
 
-  const search = () => {
+  useEffect(() => {
+    window.addEventListener('message', getMessage);
+    return () => {
+      window.removeEventListener('message', getMessage);
+    };
+  });
+
+  const isCheckAvailable = useMemo(() => {
+    return totalSelectedNodeCount > 0;
+  }, [totalSelectedNodeCount]);
+
+  const check = () => {
     dispatch(searchListenerConnect());
   };
 
-  return { filtered, search, searchDisabled };
+  return {
+    filtered,
+    isCheckAvailable,
+    check,
+  };
 };
