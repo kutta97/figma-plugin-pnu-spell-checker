078be9a3913b14a587ad5f3cfb67b203f1356915
kutta97 <yhjin04170@gmail.com>
2023-03-17 13:13:12 +0900

    feat: 선택된 노드가 없는 경우 HomeContents 변경

--- src/fragments/home/Contents.jsx
+++ src/fragments/home/Contents.jsx
@@ -3,22 +3,47 @@ import styled from 'styled-components';
 
 import { Label } from '@components/text/label';
 
-export const HomeContents = () => {
+import { useAppSelector } from '@store/hook';
+import { selectedNodeCountsSelector } from '@store/node';
+
+const DefaultHomeContents = () => {
   return (
     <HomeContentsStyled>
       <p>작업 영역에서 검사하고자 하는 텍스트가 포함된</p>
       <p>
-        <Label className="label" type="TEXT">
-          Text Layer
-        </Label>
+        <Label type="TEXT">Text Layer</Label>
         <span>혹은</span>
-        <Label className="label" type="FRAME">
-          Frame
-        </Label>
+        <Label type="FRAME">Frame</Label>
         <span>을</span>
       </p>
       <p>선택해 주세요.</p>
     </HomeContentsStyled>
+  );
+};
+
+const NodeSelectedHomeContents = () => {
+  const selectedNodeCounts = useAppSelector(selectedNodeCountsSelector);
+  return (
+    <HomeContentsStyled>
+      <p className="selected">
+        <Label type="TEXT">Text Layer</Label>
+        <span>{`${selectedNodeCounts.text}개 선택됨`}</span>
+      </p>
+      <p className="selected">
+        <Label type="FRAME">Frame</Label>
+        <span>{`${selectedNodeCounts.frame}개 선택됨`}</span>
+      </p>
+    </HomeContentsStyled>
+  );
+};
+
+export const HomeContents = (props) => {
+  const { isCheckAvailable } = props;
+
+  return isCheckAvailable ? (
+    <NodeSelectedHomeContents />
+  ) : (
+    <DefaultHomeContents />
   );
 };
 
@@ -35,5 +60,9 @@ const HomeContentsStyled = styled.div`
     display: flex;
     align-items: center;
     gap: 4px;
+  }
+
+  .selected {
+    gap: 8px;
   }
 `;
