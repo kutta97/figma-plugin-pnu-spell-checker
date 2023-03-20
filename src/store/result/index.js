1eaed216a7100842e3d46632955ca822b755bda6
kutta97 <yhjin04170@gmail.com>
2023-03-20 17:00:52 +0900

    feat: resultStore 에 검사 결과를 저장할 resultList state 추가

--- src/store/result/index.js
+++ src/store/result/index.js
@@ -1,6 +1,6 @@
 import * as selector from '@store/result/selector';
 import { resultSlice } from '@store/result/slice';
 
-export const { convertListenerConnect } = resultSlice.actions;
-export const { resultSelector } = selector;
+export const { convertListenerConnect, setResultList } = resultSlice.actions;
+export const { resultSelector, resultListSelector } = selector;
 export const resultReducer = resultSlice.reducer;
--- src/store/result/selector.js
+++ src/store/result/selector.js
@@ -1,1 +1,8 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const resultSelector = (state) => state.resultReducer;
+
+export const resultListSelector = createSelector(
+  resultSelector,
+  (state) => state.resultList
+);
