59b0f15ab98c3cbc406944eb0e916e6c3d8fa30d
kutta97 <yhjin04170@gmail.com>
2023-03-20 18:12:27 +0900

    feat: 변환 중 페이지 UI 개발

---
+++ src/fragments/converting/Contents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { EmojiProgressBarText } from '@components/text/EmojiProgressBarText';
+
+export const ConvertingContents = () => {
+  return (
+    <EmojiProgressBarText emoji="🪄">
+      <p>텍스트 변환 중이에요</p>
+    </EmojiProgressBarText>
+  );
+};
