e447c3fc20b0b0b51dd68fec9739471b146f37da
kutta97 <yhjin04170@gmail.com>
2023-03-20 18:11:54 +0900

    refactor: 변환 중 페이지 코드 개선

--- src/fragments/checking/Contents.jsx
+++ src/fragments/checking/Contents.jsx
@@ -1,38 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import { ProgressBar } from '@components/progress/ProgressBar';
-
-import { Font18W700, Font40W500 } from '@assets/styles/fonts';
+import { EmojiProgressBarText } from '@components/text/EmojiProgressBarText';
 
 export const CheckingContents = () => {
   return (
-    <CheckingContentsStyled>
-      <div className="text-container">
-        <p className="icon">👀</p>
-        <p>고칠 곳이 있는지 열심히 찾고 있어요.</p>
-      </div>
-      <ProgressBar />
-    </CheckingContentsStyled>
+    <EmojiProgressBarText emoji="👀">
+      <p>고칠 곳이 있는지 열심히 찾고 있어요.</p>
+    </EmojiProgressBarText>
   );
 };
-
-const CheckingContentsStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 17px;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-
-  .text-container {
-    gap: 12px;
-    .icon {
-      ${Font40W500}
-    }
-    p {
-      ${Font18W700}
-    }
-  }
-`;
