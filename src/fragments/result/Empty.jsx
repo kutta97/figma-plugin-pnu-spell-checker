d1aeed9707337d7c727f757f0337e8037ab237af
kutta97 <yhjin04170@gmail.com>
2023-03-23 01:15:44 +0900

    feat: 결과 없을 때 ui 개발

---
+++ src/fragments/result/Empty.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { EmojiText } from '@components/text/EmojiText';
+
+import { Font15W500 } from '@assets/styles/fonts';
+
+export const ResultEmpty = () => {
+  return (
+    <ResultEmptyStyled>
+      <EmojiText emoji="👍">
+        <p>고칠 곳이 없어요</p>
+        <p className="sub-text">
+          그래도 완벽한 작업을 위해 한 번 더 검토해 주세요 :)
+        </p>
+      </EmojiText>
+    </ResultEmptyStyled>
+  );
+};
+
+const ResultEmptyStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  .sub-text {
+    color: ${({ theme }) => theme.colors.gray50};
+    ${Font15W500}
+    margin-top: 4px;
+  }
+`;
