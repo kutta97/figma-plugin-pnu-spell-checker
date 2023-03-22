e506b298a506f267d29ff7d15815a9932e938d23
kutta97 <yhjin04170@gmail.com>
2023-03-23 02:23:11 +0900

    refactor: ResultInfo styled 객체로 변경

--- src/fragments/result/Results.jsx
+++ src/fragments/result/Results.jsx
@@ -4,10 +4,9 @@ import styled from 'styled-components';
 import { useAppSelector } from '@store/hook';
 import { resultListSelector } from '@store/result';
 
-import { Font18W700 } from '@assets/styles/fonts';
+import { Font14W500, Font15W500, Font18W700 } from '@assets/styles/fonts';
 
 import { ResultList } from '@fragments/result/ResultList';
-import { ResultInfo } from '@fragments/result/ResultsInfo';
 
 export const Results = () => {
   const resultList = useAppSelector(resultListSelector);
@@ -16,7 +15,7 @@ export const Results = () => {
 
   return (
     <ResultsStyled>
-      <ResultInfo className="info">
+      <ResultInfo>
         <p className="title">{`${resultList.length}건이 발견되었어요`}</p>
         <p>검사 결과 중 중복된 레이어들은 제거했어요</p>
         <button type="button" onClick={handleClickResultDetail}>
@@ -35,11 +34,28 @@ const ResultsStyled = styled.section`
   width: 100%;
   height: 100%;
   max-height: 548px;
+`;
 
-  .info {
-    .title {
-      color: ${({ theme }) => theme.colors.gray90};
-      ${Font18W700}
-    }
+const ResultInfo = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 0;
+  gap: 4px;
+
+  .title {
+    color: ${({ theme }) => theme.colors.gray90};
+    ${Font18W700}
+  }
+  p {
+    color: ${({ theme }) => theme.colors.gray50};
+    ${Font15W500}
+  }
+  button {
+    margin-top: 12px;
+    color: ${({ theme }) => theme.colors.gray50};
+    ${Font14W500}
+    text-decoration: underline;
   }
 `;
