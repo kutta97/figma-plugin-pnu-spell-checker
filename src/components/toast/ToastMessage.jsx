6992c10fab98cd96e776ebec30d97007d9c1bc82
kutta97 <yhjin04170@gmail.com>
2023-05-31 03:35:46 +0900

    feat: 토스트메시지 컴포넌트 추가

---
+++ src/components/toast/ToastMessage.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import WarningIcon from '@assets/icons/toast/ic_warning.svg';
+import { Font15W400 } from '@assets/styles/fonts';
+
+export const ToastMessage = (props) => {
+  const { children } = props;
+  return (
+    <ToastMessageStyled {...props}>
+      <span className="icon">
+        <WarningIcon width="24px" height="24px" />
+      </span>
+      <span className="contents">
+        <p>{children}</p>
+      </span>
+    </ToastMessageStyled>
+  );
+};
+
+const ToastMessageStyled = styled.div`
+  max-width: 100%;
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 16px;
+
+  background: rgba(69, 69, 72, 0.9);
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 8px;
+
+  position: fixed;
+  z-index: 2;
+  top: 13.5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .icon {
+    margin-right: 8px;
+  }
+
+  .contents {
+    p {
+      font-family: 'Inter';
+      ${Font15W400}
+      padding: 0;
+      white-space: nowrap;
+    }
+  }
+`;
