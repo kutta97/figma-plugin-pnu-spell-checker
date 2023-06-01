fcdb980c17ea4e8a497a2439895b676d31bd0777
Kim HunJin (Mos) <ysg01129@naver.com>
2023-06-02 01:52:23 +0900

    [NO-ISSUE] API connect (#26)
    * feat: Add gitignore
    
    * feat: Improve webpack config. Using dotenv

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,14 @@
 const { merge } = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env.dev'),
+    }),
+  ],
 });
