b646bb6cfd8b37aeaf506e6538dad8264a338b42
kutta97 <yhjin04170@gmail.com>
2023-06-01 23:14:02 +0900

    feat: 검사 결과 페이지 Result Store - 체크 기능 추가

--- src/store/result/index.js
+++ src/store/result/index.js
@@ -1,7 +1,16 @@
 import * as selector from '@store/result/selector';
 import { resultSlice } from '@store/result/slice';
 
-export const { convertListenerConnect, setResultList } = resultSlice.actions;
-export const { resultSelector, resultCountSelector, resultListSelector } =
-  selector;
+export const {
+  convertListenerConnect,
+  setResultList,
+  toggleAllResultSelection,
+  toggleResultSelection,
+} = resultSlice.actions;
+export const {
+  resultSelector,
+  resultCountSelector,
+  resultListSelector,
+  selectedResultCountSelector,
+} = selector;
 export const resultReducer = resultSlice.reducer;
