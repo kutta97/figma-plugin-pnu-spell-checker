c1be228115aeca4fac6af25051e904b7e0deeac3
HunJin Kim <ysg01129@naver.com>
2023-06-02 01:40:34 +0900

    feat: Improve webpack config. Using dotenv

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const { merge } = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env.prod'),
+    }),
+  ],
 });
