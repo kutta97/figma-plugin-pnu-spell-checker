a8c1cba86751727ee1bbd393c6412967aa33152c
kutta97 <yhjin04170@gmail.com>
2023-06-02 23:12:12 +0900

    refactor: spellCheckUsecase api response 데이터 가공 로직 함수화

--- src/data/spellCheck/spellCheckUsecase.js
+++ src/data/spellCheck/spellCheckUsecase.js
@@ -4,42 +4,47 @@
  * @constructor
  * @param api
  */
+
+const getResultDataObj = (error) => {
+  return {
+    beforeText: error.checking.text,
+    afterText: error.recommend[0],
+    help: error.errorExplanation.replaceAll('<br/>', '\n'),
+  };
+};
+
+const getResult = (data) => {
+  return {
+    results: data.errors
+      .filter((error) => error.recommend.length === 1)
+      .map((error, index) => {
+        return {
+          id: index,
+          index: {
+            startIndex: error.checking.startIndex,
+            deleteCount: error.checking.text.length,
+          },
+          data: getResultDataObj(error),
+        };
+      }),
+    resultsWithMultipleRecommends: data.errors
+      .filter((error) => error.recommend.length > 1)
+      .map((error, index) => {
+        return {
+          id: index,
+          data: getResultDataObj(error),
+        };
+      }),
+  };
+};
+
 export const SpellCheckUsecase = (api) => {
   return {
     getSpellCheck: async (originalText) => {
       const response = await api(originalText);
       const data = await response.json();
 
-      return {
-        results: data.errors
-          .filter((error) => error.recommend.length === 1)
-          .map((error, index) => {
-            return {
-              id: index,
-              index: {
-                startIndex: error.checking.startIndex,
-                deleteCount: error.checking.text.length,
-              },
-              data: {
-                beforeText: error.checking.text,
-                afterText: error.recommend[0],
-                help: error.errorExplanation.replaceAll('<br/>', '\n'),
-              },
-            };
-          }),
-        resultsWithMultipleRecommends: data.errors
-          .filter((error) => error.recommend.length > 1)
-          .map((error, index) => {
-            return {
-              id: index,
-              data: {
-                beforeText: error.checking.text,
-                afterText: error.recommend.join(', '),
-                help: error.errorExplanation.replaceAll('<br/>', '\n'),
-              },
-            };
-          }),
-      };
+      return getResult(data);
     },
   };
 };
